60ab467aa98cc321b9304019699fe44032068d4c
sebastianbarzaghi <sebastian.barzaghi@outlook.com>
2023-03-21 22:25:16 +0100

    fix: fix exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let total = 0;
+    for (let i = start; i <= end; i++) {
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
