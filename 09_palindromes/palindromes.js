8e302c621ad9bebfec313a360e8d28017d8e3a3f
sebastianbarzaghi <sebastian.barzaghi@outlook.com>
2023-04-12 22:56:10 +0200

    fix: solve exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let re = /[\W]/g;
+    let lowString = string.toLowerCase().replace(re, "");
+    let reverseString = lowString.split("").reverse().join(""); 
+    if (lowString === reverseString) return true;
 };
 
 // Do not edit below this line
